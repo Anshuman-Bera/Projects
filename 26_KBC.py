737f1da1e6946d6cd8d0f6e268549ac80184c71f
Anshuman Bera <161289762+Anshuman-Bera@users.noreply.github.com>
2024-03-04 23:18:37 +0530

    Add files via upload

---
+++ 26_KBC.py
@@ -0,0 +1,57 @@
+question =[
+  ["The Central Rice Research Station is situated in?",
+    "Chennai","Cuttack","Bangalore","Quilon",2],
+  
+  ["The metal whose salts are sensitive to light is?",
+   "Zinc","Silver","Copper","Aluminum",2],
+  
+  ["Which language is use for make telegram?" 
+    ,"sql","React","Next js","flater",2],
+
+  ["Which language is use for make instagram?" 
+    ,"python","js","C++","php",1],
+  
+  ["Which language is use for make facebook?" 
+    ,"python","js","C++","php",3],
+  
+  ["Which language is use for make facebook?" 
+    ,"python","js","C++","php",2],
+  
+  ["Which language is use for make facebook?" 
+    ,"python","js","C++","php",3],
+  
+  ["Which language is use for make facebook?" 
+    ,"python","js","C++","php",2],
+  
+  ["Which language is use for make facebook?" 
+    ,"python","js","C++","php",4],
+  
+  ["Which language is use for make facebook?" 
+    ,"python","js","C++","php",3],
+  
+  ["Which language is use for make facebook?" 
+    ,"python","js","C++","php",1]
+  ]
+
+money =[1000,2000,3000,4000,5000,10000,20000,40000,80000,160000,320000,600000]
+prize = 0
+for i in range(0,len(question)):
+  questions = question[i]
+  print(f"\n\nThe question for {money[i]} money :- ")
+  print(questions[0]) 
+  print(f"1.{questions[1]}       2.{questions[2]}")
+  print(f"3.{questions[3]}       4.{questions[4]}")
+  Answer = (input("Enter your answer "))
+  if(Answer == "Cuttack") or (Answer == "Silver") or (Answer == 
+    "React") or (Answer =="C++"):
+  # if(Answer==questions[-2]) or (Answer ==
+  #      questions[-2]) or (Answer == questions[-3]) or (Answer == questions[-3]):
+  #   print(f"Correct answer and you own {money[i]} rupees")
+    if (i == 4):
+     prize = 5000
+    elif (i == 9):
+     prize = 320000
+  else:
+    print("Wrong answer")
+    break
+print(f"Your take home money is {prize}")
